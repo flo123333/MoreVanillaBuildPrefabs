ce2c3110c043885ab64391497eb80027e320d25e
searica <143636061+searica@users.noreply.github.com>
2024-05-15 17:25:10 -0700

    Make config generation more dynamic and handle new settings

--- Python/MVBP_Config_to_DefaultConfigs.py
+++ Python/MVBP_Config_to_DefaultConfigs.py
@@ -1,10 +1,38 @@
+import os
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 
 class PrefabConfig:
     """Class to hold prefab configuration settings"""
-    tab = "    "
+    _tab = "    "
+
+    _ordering = {
+        "name": 1,
+        "enabled": 2,
+        "allowedInDungeons": 3,
+        "category": 4,
+        "craftingStation": 5,
+        "requirements": 6,
+        "clipEverything": 7,
+        "clipGround": 8,
+        "placementPatch": 9,
+        "placementOffset": 10,
+        "pieceName": 11,
+        "pieceDesc": 12,
+        "pieceGroup": 13,
+        "playerBasePatch": 14,
+        "spawnOnDestroyed": 15,
+    }
+
+    _needs_quotes = set(
+        [
+            "name",
+            "requirements",
+            "pieceDesc",
+            "pieceName"
+        ]
+    )
 
     def __init__(self):
         self.name = None
@@ -16,56 +44,38 @@ class PrefabConfig:
         self.clipEverything = None
         self.clipGround = None
         self.placementPatch = None
+        self.placementOffset = None
         self.pieceName = None
         self.pieceDesc = None
         self.pieceGroup = None
         self.playerBasePatch = None
+        self.spawnOnDestroyed = None
 
-    def iter_attr(self):
-        return [x for x in dir(self) if not x.startswith('__') and not callable(getattr(self, x))]
+    def _sort_attr_names(self, name: str):
+        if name not in self._ordering:
+            raise KeyError(f"Could not find ordering value for: {name}")
+        return self._ordering[name]
+
+    def get_attr_names(self) -> List[str]:
+        names = [x for x in dir(self) if self.__is_attr(x)]
+        names.sort(key=self._sort_attr_names)
+        return names
+
+    def __is_attr(self, name: str) -> bool:
+        return not name.startswith('_') and not callable(getattr(self, name))
 
     def is_valid(self):
         return self.name is not None
 
     def __str__(self):
         result = ["new PrefabDB("]
-        if self.name is not None:
-            result.append(f'{self.tab}name: "{self.name}",')
 
-        if self.enabled is not None:
-            result.append(f'{self.tab}enabled: {self.enabled},')
-
-        if self.allowedInDungeons is not None:
-            result.append(
-                f'{self.tab}allowedInDungeons: {self.allowedInDungeons},'
-            )
-
-        if self.category is not None:
-            result.append(f'{self.tab}category: {self.category},')
-
-        if self.craftingStation is not None:
-            result.append(f'{self.tab}craftingStation: {self.craftingStation},')
-
-        if self.requirements is not None:
-            result.append(f'{self.tab}requirements: "{self.requirements}",')
-
-        if self.clipEverything is not None:
-            result.append(f'{self.tab}clipEverything: {self.clipEverything},')
-
-        if self.clipGround is not None:
-            result.append(f'{self.tab}clipGround: {self.clipGround},')
-
-        if self.pieceName is not None:
-            result.append(f'{self.tab}pieceName: "{self.pieceName}",')
-
-        if self.pieceDesc is not None:
-            result.append(f'{self.tab}pieceDesc: "{self.pieceDesc}",')
-
-        if self.pieceGroup is not None:
-            result.append(f'{self.tab}pieceGroup: {self.pieceGroup},')
-
-        if self.playerBasePatch is not None:
-            result.append(f'{self.tab}playerBasePatch: {self.playerBasePatch},')
+        for name in self.get_attr_names():
+            if getattr(self, name) is not None:
+                val = getattr(self, name)
+                if name in self._needs_quotes:
+                    val = f'\"{val}\"'
+                result.append(f'{self._tab}{name}: {val},')
 
         result[-1] = result[-1].strip(",")
         result.append(")")
@@ -73,9 +83,20 @@ class PrefabConfig:
 
 
 def main():
-    default_config_path = Path(r"C:\Users\TheUser\Documents\Projects\ValheimMods\MoreVanillaBuildPrefabs\Configs\PrefabConfigs.cs")
+    default_config_path = Path(__file__).parents[1].joinpath(
+        "Configs",
+        "PrefabDefaults.cs"
+    )
 
-    cfg_file_path = Path(r"C:\Users\TheUser\AppData\Roaming\r2modmanPlus-local\Valheim\profiles\Mod-Debug\BepInEx\config\Searica.Valheim.MoreVanillaBuildPrefabs.cfg")
+    cfg_file_path = Path(os.getenv('APPDATA')).joinpath(
+        "r2modmanPlus-local",
+        "Valheim",
+        "profiles",
+        "Mod-Debug",
+        "BepInEx",
+        "config",
+        "Searica.Valheim.MoreVanillaBuildPrefabs.cfg"
+    )
 
     out_path = Path(__file__).parent.joinpath("DefaultConfigs.txt")
 
@@ -90,18 +111,20 @@ def main():
     for key, val in prefab_configs.items():
         if key not in default_configs:
             default_configs.update({key: val})
-            print("Added prefab")
+            print(f"Added Prefab: {key}")
             print(val)
         else:
-            for attr in val.iter_attr():
+            for attr in val.get_attr_names():
                 attr_val = getattr(val, attr)
                 if attr_val is not None:
                     current_val = getattr(default_configs[key], attr)
                     if current_val != attr_val:
                         setattr(default_configs[key], attr, attr_val)
                         if current_val is not None:
-                            print("Prefab:", key)
-                            print(f"Changed {attr}: {current_val} to {attr_val}")
+                            print("Modified Prefab:", key)
+                            print(
+                                f"Changed {attr}: {current_val} to {attr_val}"
+                            )
 
     # write modified default configs to output
     write_output(out_path, default_configs)
@@ -119,61 +142,25 @@ def read_default_prefabs(file_path, start_line) -> Dict[str, PrefabConfig]:
         while (lines[i] != start_line):
             i = i + 1
 
+        prefab_config = PrefabConfig()
+
         for line in lines[i:]:
             if "name:" in line:
-                # add config to dictionary is it has been initialized
+                # Encountering a "name:" line when prefab_config is valid
+                # means this is a new prefab config settings so cache the
+                # current prefab_config and make a new one
                 if (prefab_config.is_valid()):
                     default_configs.update({prefab_config.name: prefab_config})
-                    prefab_config = PrefabConfig()
+                prefab_config = PrefabConfig()
                 prefab_config.name = get_line_value(line, "name:")
 
-            if "enabled:" in line:
-                prefab_config.enabled = get_line_value(line, "enabled:")
-
-            if "allowedInDungeons:" in line:
-                prefab_config.allowedInDungeons = get_line_value(
-                    line, "allowedInDungeons:"
-                )
-
-            if "category:" in line:
-                prefab_config.category = get_line_value(line, "category:")
-
-            if "craftingStation:" in line:
-                prefab_config.craftingStation = get_line_value(
-                    line, "craftingStation:"
-                )
-
-            if "requirements:" in line:
-                prefab_config.requirements = get_line_value(
-                    line, "requirements:"
-                )
-
-            if "clipEverything:" in line:
-                prefab_config.clipEverything = get_line_value(
-                    line, "clipEverything:"
-                )
-
-            if "clipGround:" in line:
-                prefab_config.clipGround = get_line_value(
-                    line, "clipGround:"
-                )
-
-            if "placementPatch:" in line:
-                prefab_config.placementPatch = get_line_value(
-                    line, "placementPatch:"
-                )
-
-            if "pieceName:" in line:
-                prefab_config.pieceName = get_line_value(line, "pieceName:")
-
-            if "pieceDesc:" in line:
-                prefab_config.pieceDesc = get_line_value(line, "pieceDesc:")
-
-            if "pieceGroup:" in line:
-                prefab_config.pieceGroup = get_line_value(line, "pieceGroup:")
-
-            if "playerBasePatch" in line:
-                prefab_config.playerBasePatch = get_line_value(line, "playerBasePatch:")
+            for name in prefab_config.get_attr_names():
+                if f"{name}:" in line:
+                    setattr(
+                        prefab_config,
+                        name,
+                        get_line_value(line, f"{name}:")
+                    )
 
         if (prefab_config.is_valid()):
             default_configs.update({prefab_config.name: prefab_config})
@@ -189,52 +176,21 @@ def read_config_file(file_path) -> Dict[str, PrefabConfig]:
 
     with open(file_path, "r") as file:
         lines = file.readlines()
-        for line in lines[72:]:
-            if line.startswith("["):
+
+        prefab_config = PrefabConfig()
+
+        for line in lines[88:]:
+            if line.startswith("["):  # prefab config section
                 # add config to dictionary is it has been initialized
                 if (prefab_config.is_valid()):
                     prefab_configs.update({prefab_config.name: prefab_config})
                     prefab_config = PrefabConfig()
-                prefab_config.name = line[1:-2]
+                prefab_config.name = line[1:-2]  # strip ["name"]\n
 
-            if "Enabled = " in line:
-                prefab_config.enabled = get_line_value(line, "=")
-
-            if "AllowedInDungeons = " in line:
-                prefab_config.allowedInDungeons = get_line_value(line, "=")
-
-            if "Category = " in line:
-                val = get_line_value(line, "=")
-                prefab_config.category = f"HammerCategories.{val}"
-
-            if "CraftingStation = " in line:
-                val = get_line_value(line, "=")
-                val = f"nameof(CraftingStations.{val})"
-                prefab_config.craftingStation = val
-
-            if "Requirements = " in line:
-                prefab_config.requirements = get_line_value(line, "=")
-
-            if "ClipEverything = " in line:
-                prefab_config.clipEverything = get_line_value(line, "=")
-
-            if "ClipGround = " in line:
-                prefab_config.clipGround = get_line_value(line, "=")
-
-            if "PlacementPatch = " in line:
-                prefab_config.placementPatch = get_line_value(line, "=")
-
-            if "PieceName = " in line:
-                prefab_config.pieceName = get_line_value(line, "=")
-
-            if "PieceDesc = " in line:
-                prefab_config.pieceDesc = get_line_value(line, "=")
-
-            if "PieceGroup = " in line:
-                prefab_config.pieceGroup = get_line_value(line, "=")
-
-            if "PlayerBasePatch = " in line:
-                prefab_config.pieceGroup = get_line_value(line, "=")
+            for name in prefab_config.get_attr_names():
+                line_start = f"{captialize_first_letter(name)} = "
+                if line.startswith(line_start):
+                    setattr(prefab_config, name, get_line_value(line, "="))
 
     if (prefab_config.is_valid()):
         prefab_configs.update({prefab_config.name: prefab_config})
@@ -242,9 +198,22 @@ def read_config_file(file_path) -> Dict[str, PrefabConfig]:
     return prefab_configs
 
 
+def captialize_first_letter(text: str):
+    first = text[0].capitalize()
+    return first + text[1:]
+
+
 def get_line_value(line: str, split: str):
     """Gets value from line after the split string"""
-    return line.split(split)[-1].strip(",\n").strip().strip('"')
+    val = line.split(split)[-1].strip(",\n").strip().strip('"')
+    val = val.replace("BuildingWorkbench", "Building")
+    val = val.replace("BuildingStonecutter", "Stonecutter")
+    if "Category = " in line:
+        return f"HammerCategories.{val}"
+    if "CraftingStation = " in line:
+        val = f"nameof(CraftingStations.{val})"
+
+    return val
 
 
 def write_output(out_path: Path, prefab_configs: Dict[str, PrefabConfig]):
@@ -268,48 +237,3 @@ def write_output(out_path: Path, prefab_configs: Dict[str, PrefabConfig]):
 
 if __name__ == "__main__":
     main()
-
-
-
-# with open(out_path, "w") as out_file:
-#     i = 35
-
-#     tab = "    "
-#     out_file.write(tab*2 + "internal static readonly Dictionary<string, PrefabDB> DefaultConfigValues = new()\n")
-#     out_file.write(tab*2 + "{\n")
-
-#     nature_pieces = []
-#     creator_shop_pieces = []
-
-#     while i < len(lines):
-#         line = lines[i]
-
-
-#         if "Enabled = " in line:
-#             val = line.split(" ")[-1].strip("\n")
-#             out_file.write(tab*5 + f'{val},\n')
-
-#         if line.startswith("AllowedInDungeons = "):
-#             val = line.split(" ")[-1].strip("\n")
-#             out_file.write(tab*5 + f'{val},\n')
-
-#         if line.startswith("Category = "):
-#             val = line.split(" ")[-1].strip("\n")
-#             out_file.write(tab*5 + f'HammerCategories.{val},\n')
-#             if (val == "Nature"):
-#                 nature_pieces.append(name)
-#             if (val == "CreatorShop"):
-#                 creator_shop_pieces.append(name)
-
-#         if line.startswith("CraftingStation = "):
-#             val = line.split(" ")[-1].strip("\n")
-#             out_file.write(tab*5 + f'nameof(CraftingStations.{val}),\n')
-
-#         if line.startswith("Requirements = "):
-#             val = line.split(" ")[-1].strip("\n")
-#             out_file.write(tab*5 + f'\"{val}\"\n')
-#             out_file.write(tab*4 + ")\n")
-#             out_file.write(tab*3 + "},\n")
-
-#         i = i + 1
-#     out_file.write(tab*2 + "};")
