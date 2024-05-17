37657be3e45506dd44fea8b9bd9bd3430803976f
searica <143636061+searica@users.noreply.github.com>
2024-05-17 12:00:30 -0700

    Add mesh and collider bounds methods

--- Helpers/CollisionHelper.cs
+++ Helpers/CollisionHelper.cs
@@ -1,6 +1,7 @@
 ﻿// Ignore Spelling: MVBP
 
 using System.Collections.Generic;
+using Jotunn;
 using UnityEngine;
 
 namespace MVBP.Helpers
@@ -14,16 +15,73 @@ namespace MVBP.Helpers
             collider.size = size;
         }
 
+
+        /// <summary>
+        ///     Adds a box collider to the prefab based on rendering bounds
+        ///     of the object
+        /// </summary>
+        /// <param name="prefab"></param>
         internal static void PatchCollider(GameObject prefab)
         {
             // Needed to make some things work, like Stalagmite, Rock_destructible, Rock_7, silvervein, etc.
+            var desiredBounds = GetRendererBounds(prefab);
+            AddBoxCollider(prefab, desiredBounds.center, desiredBounds.size);
+        }
+
+
+        /// <summary>
+        ///     Creates a bounding box that encapsulating the bounds of
+        ///     all Renderer's attached to the GameObject
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        internal static Bounds GetRendererBounds(GameObject prefab){
             Bounds desiredBounds = new();
             foreach (Renderer renderer in prefab.GetComponentsInChildren<Renderer>())
             {
                 desiredBounds.Encapsulate(renderer.bounds);
             }
-            AddBoxCollider(prefab, desiredBounds.center, desiredBounds.size);
+            return desiredBounds;
         }
+
+
+        /// <summary>
+        ///     Creates a bounding box that encapsulating the bounds of
+        ///     all Meshes attached to the GameObject via MeshFilter or
+        ///     SkinnedMeshRenderer components
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        internal static Bounds GetMeshBounds(GameObject prefab){
+            Bounds desiredBounds = new();
+            foreach (MeshFilter meshFilter in prefab.GetComponentsInChildren<MeshFilter>())
+            {
+                desiredBounds.Encapsulate(meshFilter.mesh.bounds);
+            }
+
+            foreach (SkinnedMeshRenderer skinMeshRender in prefab.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                desiredBounds.Encapsulate(skinMeshRender.mesh.bounds);
+            }
+            return desiredBounds;
+        }
+
+        /// <summary>
+        ///     Creates a bounding box that encapsulating the bounds of
+        ///     all Colliders attached to the GameObject that are active
+        ///     and not trigger colliders
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        internal static Bounds GetColliderBounds(GameObject prefab){
+            Bounds desiredBounds = new();
+            foreach (Collider collider in GetAllColliders(prefab))
+            {
+                desiredBounds.Encapsulate(collider.bounds);
+            }
+            return desiredBounds;
+        }
+
 
         internal static void RemoveColliders(GameObject prefab)
         {
